40acc597c2c05b8f10a2972842fd2a9a470037b3
Levi Shevick <levidshevick@gmail.com>
2022-06-02 15:36:50 -0400

    adds css for blog reading app

--- src/components/BlogReadingApp.js
+++ src/components/BlogReadingApp.js
@@ -15,20 +15,20 @@ const BlogReadingApp = () => {
             <div className="blog-display">
 
                 {selection === 'blogOne' &&
-                    <div className="blog-one">
+                    <div className="blog">
                         <h2>Blog One</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                     </div>
                 }
                 {selection === 'blogTwo' &&
-                    <div className="blog-two">
+                    <div className="blog">
                         <h2>Blog Two</h2>
                         <p>Post-ironic air plant bicycle rights bespoke gluten-free waistcoat vexillologist. Lo-fi tilde adaptogen tofu. Disrupt keffiyeh retro cloud bread yes plz snackwave tumblr pabst aesthetic organic wayfarers pug jianbing taiyaki. Selfies hot chicken fanny pack, pitchfork authentic gochujang celiac. Blog wayfarers tousled readymade, ethical yr helvetica taiyaki typewriter chicharrones YOLO wolf.</p>
                     </div>
                 }
 
                 {selection === 'blogThree' &&
-                    <div className="blog-three">
+                    <div className="blog">
                         <h2>Blog Three</h2>
                         <p>Lorem crackalackin hizzle for sure amizzle, cool izzle elit. Black fizzle velizzle, i'm in the shizzle volutpizzle, suscipizzle nizzle, we gonna chung funky fresh, fo shizzle. Pellentesque stuff tortizzle. Pot eros. Crazy bizzle dolor sizzle fo shizzle tempizzle the bizzle. Gangster uhuh ... yih! nibh izzle turpizzle. Fo shizzle in yippiyo. Pellentesque mammasay mammasa mamma oo sa rhoncizzle bizzle. In hac break yo neck, yall platea for sure. Pot dapibizzle. Shizznit tellizzle tellivizzle, pretizzle mammasay mammasa mamma oo sa, mattizzle uhuh ... yih!, gangsta vitae, nunc. I saw beyonces tizzles and my pizzle went crizzle suscipizzle. Integer semper phat sizzle tellivizzle.</p>
                     </div>
